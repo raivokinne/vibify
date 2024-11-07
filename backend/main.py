24f74b9e70e02fa6ae2fdf4c19718cb9b3048fc0
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-11-07 09:20:29 +0200

    upadte

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=3333)
